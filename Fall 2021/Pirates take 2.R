4d907d5fa6f81a6a55b551c4ee8f63e5772363f4
Fawn-Rose <nova-rose.brown@utexas.edu>
2021-10-26 03:10:12 -0500

    10/25 work session getting back into the swing of things with coding!

--- Fall 2021/Pirates take 2.R
+++ Fall 2021/Pirates take 2.R
@@ -49,4 +49,90 @@ pirates %>%
   group_by(sex) %>%
     summarize(avg_age_by_sex = mean(age))
 
-#17: 
+#17: summary stats for all columns
+summary(pirates)
+#age range: 11 to 46
+
+
+#Data Plotting: Scatter Plot
+
+#19-21
+weight_vs_height <- ggplot(pirates,aes(x=weight, y = height)) +
+  labs(title = "Weight vs Height of Pirates", x = "Weight (kg)", y = "Height (cm)") +
+  geom_point(alpha=0.5) +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+#the above line adjust the title to be in the center and makes the text size 12pt
+#alpha argument adjusts how see-through the points are
+weight_vs_height
+
+#22: previous qs with line of best fit
+weight_vs_height <- ggplot(pirates,aes(x=weight, y = height)) +
+  labs(title = "Weight vs Height of Pirates", x = "Weight (kg)", y = "Height (cm)") +
+  geom_point(alpha=0.5) +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12)) +
+#the above line adjust the title to be in the center and makes the text size 12pt
+  geom_smooth(method="lm",se=FALSE)
+#geom_smooth makes a line of best fit, method='lm' for linear model, se is hiding confidence interval
+#alpha argument adjusts how see-through the points are
+weight_vs_height
+
+#23: age vs parrot ownership
+age_vs_parrots <- ggplot(pirates,aes(x=age,y=parrots)) +
+  labs(title = "Age vs Parrot Ownership", x = "Age (years)", y = "How many parrots do you own?") +
+  geom_point(pch=16,col=gray(level=0.5,alpha=0.6)) +
+  theme_classic() +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+age_vs_parrots
+
+
+#Data Plotting: Bar Graphs
+
+#24: two ways of making bar graphs
+#first is geom_bar() for histograms
+#second is geom_col()
+myplot <- ggplot(data=pirates,aes(sex)) +
+  geom_bar(fill="#bf5700") +
+  labs(title = "How many of each sex are in this pirate crew?") +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+myplot
+#fill command in geom_bar changes what color the bars are filled with
+
+#25: adding additional category
+myplot <- ggplot(data=pirates,aes(sex)) +
+  geom_bar(aes(fill=headband)) +
+  labs(title = "How many of each sex are in this pirate crew?") +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+myplot
+#^filling bar with headband yes/no for each sex
+
+myplot <- ggplot(data=pirates,aes(sex)) +
+  geom_bar(aes(fill=headband),position="dodge") +
+  labs(title = "How many of each sex are in this pirate crew?") +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+myplot
+#^two different bars for headband yes/no
+
+myplot <- ggplot(data=pirates,aes(sex)) +
+  geom_bar(aes(fill=headband),position="fill") +
+  labs(title = "How many of each sex are in this pirate crew?") +
+  theme(plot.title = element_text(hjust=0.5, size=14), axis.title = element_text(size=12))
+myplot
+#^percentage of each sex with headband yes or headband no
+
+#26: geom_col
+avg_beard_by_sex <- pirates %>%
+  group_by(sex) %>%
+    summarize(average = mean(beard.length), stdev = sd(beard.length))
+View(avg_beard_by_sex)
+
+beard_vs_sex_column <- ggplot(data=avg_beard_by_sex,aes(x=sex,y=average)) +
+  geom_col() +
+  labs(title = "Average beard length by sex")
+beard_vs_sex_column
+
+#27: adding error bar
+beard_vs_sex_column <- ggplot(data=avg_beard_by_sex,aes(x=sex,y=average)) +
+  geom_col() +
+  labs(title = "Average beard length by sex") +
+  geom_errorbar()
+beard_vs_sex_column
