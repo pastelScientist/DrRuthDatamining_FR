5a7c6c2473b3908c2552d99471c79692aeb92aea
Fawn-Rose <nova-rose.brown@utexas.edu>
2021-12-20 21:19:55 -0600

    RSQA Take 2, day 1!!

---
+++ Fall 2021/RSQA Worksheet Take 2/RSQA Worksheet Take 2.R
@@ -0,0 +1,18 @@
+##Downloading Data (see procedure for RSQA website steps)
+
+#2: for downloaded data, picking California pesticides data
+
+#see RSQA worksheet for where to find different types of info within RSQA data downloads
+
+
+##Getting Set Up
+
+#7: making this file + setting working directory
+setwd("C:/Users/Fawn-Rose/Documents/20XX College/Urban Ecosystems/DrRuthDatamining_FR/Fall 2021/RSQA Worksheet Take 2")
+getwd()
+
+
+##Loading Data into R
+library(readr)
+library(dplyr)
+library(ggplot2)
