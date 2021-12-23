be2866310eea299b75fa7b43f19d4fd023ca879c
Fawn-Rose <nova-rose.brown@utexas.edu>
2021-12-23 17:33:26 -0600

    12.23 RSQA work session! Facet plots: not sure what's going on with my axes but will figure it out with a fresh brain next time

--- Fall 2021/RSQA Worksheet Take 2/RSQA Worksheet Take 2.R
+++ Fall 2021/RSQA Worksheet Take 2/RSQA Worksheet Take 2.R
@@ -44,3 +44,45 @@ head(expcount)
 #15: Myclobutanil, wf dataframe
 Myclobutanil_data <- mydata[mydata$PARM_NM == "Myclobutanil, wf",]
 
+#16: Myclobutanil info in lab notebook
+
+#18: scatterplot of RESULT_VA of Myclobutanil data
+myclobutanil_plot <- ggplot(data=Myclobutanil_data, aes(x=PARM_NM, y=RESULT_VA)) +
+    geom_point() +
+    labs(title = "Myclobutanil concentration summary")
+myclobutanil_plot
+
+#19: JITTERPLOT of RESULT_VA of Myclubtanil data
+myclobutanil_plot <- ggplot(data=Myclobutanil_data, aes(x=PARM_NM, y=RESULT_VA)) +
+  geom_jitter() +
+  labs(title = "Myclobutanil concentration summary", x = "Myclobutanil, wf", y = "Concentration detected (ng/L)")+
+  theme_classic() +
+  theme(axis.text.x =element_blank())
+myclobutanil_plot
+
+
+##Plotting multiple parameters
+#21:
+top_5_data <- mydata[mydata$PARM_NM %in% expcount$PARM_NM[1:5], ]
+
+#22: jitterplot of top_5_data
+top_5_data_plot <- ggplot(data=top_5_data, aes(x=PARM_NM, y=RESULT_VA, color=PARM_NM)) +
+  geom_jitter() +
+  labs(title = "Top 5 Most Detected Pesticides", x = "Pesticide name", y = "Concentration detected (ng/L)")+
+  theme_classic() +
+  theme(axis.text.x = element_blank())
+top_5_data_plot
+
+#24: facet plot
+top_5_data_plot <- ggplot(data=top_5_data, aes(x=PARM_NM, y=RESULT_VA)) +
+  geom_jitter() +
+  facet_wrap(. ~PARM_NM, scales = "free") +
+  labs(title = "Top 5 Most Detected Pesticides", x = "Pesticide name", y = "Concentration detected (ng/L)")+
+  theme_classic() +
+  theme(axis.text.x = element_blank())
+top_5_data_plot
+
+#lol looks like 3 of the 5 most detected are actually all nondetects (just looking qualitatively)
+
+
+##Dealing with non-detects
